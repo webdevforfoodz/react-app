ad289b07621a9df595ea1001fc49338356091fa2
webdevforfoodz <73584854+webdevforfoodz@users.noreply.github.com>
2020-11-27 20:03:33 -0800

    update

--- src/routes/FirstRoute.js
+++ src/routes/FirstRoute.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Waifu from '../assets/logo-holding-waifu.png'
 import logo from '../assets/logo.png'
+import '../css/firstroute.css'
 
 function FirstRoute() {
     return(
         <div>
             <div className="first-route-waifu">
-                <img src={logo} className="first-route-logo" alt="logo" style={{width: '250px'}}/>
-                <img src={Waifu} alt="first-route-waifu" />
+                <img src={logo} className="first-route-logo" alt="logo"/>
+                <img src={Waifu} className="first-route-waifu-img" alt="first-route-waifu" />
             </div>
         </div>
     )
